b7041baaa2c581819d5144c3e9c51eeb5a974020
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2024-12-12 18:29:14 -0500

    primer commit

---
+++ src/main/java/com/demo/FacialHealthApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacialHealthApplication.class, args);
+	}
+
+}
